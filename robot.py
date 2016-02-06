581956cdf18d69358ca56cc1e49117be07b9e264
Bill <wgray@westburyschools.org>
2016-02-06 11:15:21 -0500

    added shooter, elevator,shooter angle,.

--- robot.py
+++ robot.py
@@ -24,6 +24,14 @@ class MyRobot(wpilib.IterativeRobot):
         self.aimer = wpilib.Talon(6)
         self.loader = wpilib.Talon(7)
 
+        # initiating the lift
+        self.liftMotor = wpilib.Talon(8)
+        self.liftMotor2 = wpilib.Talon(9)
+
+        # Adjust the angle of the shooter
+        self.shooterAngle = wpilib.Talon(10)
+
+
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
         self.auto_loop_counter = 0
@@ -42,13 +50,38 @@ class MyRobot(wpilib.IterativeRobot):
         """This function is called periodically during operator control."""
         self.robot_drive.arcadeDrive(self.stick)
 
-        # if button A is pushed, Spin Shooter so it can take a ball in.
+        # if button A is pushed, then Spin Shooter will take a ball in.
         if self.stick.getRawButton(1):
             self.leftSpinner.set(-1.0)
             self.rightSpinner.set(-1.0)
+            self.loader.set(-1.0)
+        elif self.stick.getRawButton(2):  # if button B is pushed, ball shoots out.
+            self.leftSpinner.set(1.0)
+            self.rightSpinner.set(1.0)
+            self.loader.set(1.0)
         else:
             self.leftSpinner.set(0)
             self.rightSpinner.set(0)
+            self.loader.set(0)
+
+        # if Button X is pushed, then the elevator will go down
+        if self.stick.getRawButton(3):
+            self.liftMotor.set(-1.0)
+            self.liftMotor2.set(-1.0)
+        elif self.stick.getRawButton(4):  # if button Y is pushed, then the elevator will go up
+            self.liftMotor.set(1.0)
+            self.liftMotor2.set(1.0)
+        else:
+            self.liftMotor.set(0)
+            self.liftMotor2.set(0)
+
+        # if Left Bumper is pushed, then the shooter angle down
+        if self.stick.getRawButton(5):
+            self.shooterAngle.set(-1.0)
+        elif self.stick.getRawButton(6):  # if right bumper pushed, shooter angle up
+            self.shooterAngle.set(1.0)
+        else:
+            self.shooterAngle.set(0)
 
     def testPeriodic(self):
         """This function is called periodically during test mode."""
