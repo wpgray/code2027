f35b7d3ba141ad943e985ee3663f28bf215dca47
Luis Tolosa <let2120@columbia.edu>
2016-02-16 14:02:20 -0500

    Figured out how to get autocomplete to work!

--- opencv_install/CVTest.py
+++ opencv_install/CVTest.py
@@ -1,14 +1,14 @@
 import cv2
 
 
-#Read the image file
+# Read the image file
 img = cv2.imread('original_phpS63uWD.fig', 1)
 
-#Create window and display the image with title 'Hello World'
+# Create window and display the image with title 'Hello World'
 cv2.imshow('Hello World', img)
 
-#Hold the window until closed
+# Hold the window until closed
 cv2.waitKey(0)
 
-#Close window
+# Close window
 cv2.destroyAllWindows()
